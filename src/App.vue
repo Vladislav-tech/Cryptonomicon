1f72c03e812c5a171b8edab37eb0b0a921615079
vladislav <vladislav.frint@gmail.com>
2021-08-11 12:45:31 +0300

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -60,126 +60,129 @@
         </button>
         <transition-group tag="div" name="bounce">
           <template v-if="tickers.length">
-          <button 
-            class="bg-blue-500 hover:bg-blue-400 text-white mr-2 font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-            v-for="currency in currencies" :key="currency"
-            @click="switchCurrency(currency)"
+            <button
+              class="bg-blue-500 hover:bg-blue-400 text-white mr-2 font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+              v-for="currency in currencies"
+              :key="currency"
+              @click="switchCurrency(currency)"
             >
-            {{ currency }}
-          </button>      
+              {{ currency }}
+            </button>
           </template>
         </transition-group>
       </section>
 
       <transition-group name="fade">
-      <template v-if="tickers.length">
-        <hr class="w-full border-t border-gray-600 my-4" />
-        <p>
-          Фильтры:
-          <input
-            v-model="filterName"
-            type="text"
-            placeholder="Имя"
-            class="block border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-          />
-          <button
-            v-if="page > 1"
-            @click="page--"
-            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-          >
-            Назад
-          </button>
-          <button
-            v-if="hasNextPage"
-            @click="page++"
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-          >
-            Вперёд
-          </button>
-        </p>
-        <p>
-          Сортировка
-          <select
-            id="sortings"
-            v-model="sorting"
-            class="block border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-          >
-            >
-            <option value="name A-Z">По названию</option>
-            <option value="name Z-A">По названию (обратный порядок)</option>
-            <option value="price ascending">Цена (возрастание)</option>
-            <option value="price descending">Цена (убывание) </option>
-          </select>
-        </p>
-        <button 
-          type="button"
-          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-          @click="clearTickers"
-          >Очистить</button>
-        <hr class="w-full border-t border-gray-600 my-4" />
-        <transition-group
-          name="list"
-          tag="dl"
-          class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3"
-        >
-          <div
-            v-for="t in filteredTickers()"
-            :key="t.name"
-            @click="select(t)"
-            :class="{
-              'border-4': sel === t
-            }"
-            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-          >
-            <div class="px-4 py-5 sm:p-6 text-center">
-              <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ t.name }} - {{ choosedCurrency }}
-              </dt>
-              <span class="flex items-center justify-center">
-                <img :src="t.image" />
-              </span>
-              <dd class="mt-1 text-2xl font-semibold text-green-600">
-                {{ numberWithSpaces(t.price) }}
-              </dd>
-            </div>
-            <div class="w-full border-t border-gray-200"></div>
+        <template v-if="tickers.length">
+          <hr class="w-full border-t border-gray-600 my-4" />
+          <p>
+            Фильтры:
+            <input
+              v-model="filterName"
+              type="text"
+              placeholder="Имя"
+              class="block border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+            />
             <button
-              @click.stop="handleDelete(t)"
-              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+              v-if="page > 1"
+              @click="page--"
+              class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
             >
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#718096"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd"
-                ></path></svg
-              >Удалить
+              Назад
             </button>
-          </div>
-        </transition-group>
-        <hr class="w-full border-t border-gray-600 my-4" />
-      </template>
+            <button
+              v-if="hasNextPage"
+              @click="page++"
+              class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            >
+              Вперёд
+            </button>
+          </p>
+          <p>
+            Сортировка
+            <select
+              id="sortings"
+              v-model="sorting"
+              class="block border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+            >
+              >
+              <option value="name A-Z">По названию</option>
+              <option value="name Z-A">По названию (обратный порядок)</option>
+              <option value="price ascending">Цена (возрастание)</option>
+              <option value="price descending">Цена (убывание) </option>
+            </select>
+          </p>
+          <button
+            type="button"
+            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            @click="clearTickers"
+          >
+            Очистить
+          </button>
+          <hr class="w-full border-t border-gray-600 my-4" />
+          <transition-group
+            name="list"
+            tag="dl"
+            class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3"
+          >
+            <div
+              v-for="t in paginatedTickers"
+              :key="t.name"
+              @click="select(t)"
+              :class="{
+                'border-4': selectedTicker === t
+              }"
+              class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+            >
+              <div class="px-4 py-5 sm:p-6 text-center">
+                <dt class="text-sm font-medium text-gray-500 truncate">
+                  {{ t.name }} - {{ choosedCurrency }}
+                </dt>
+                <span class="flex items-center justify-center">
+                  <img class="ticker-img" :src="t.image" />
+                </span>
+                <dd class="mt-1 text-2xl font-semibold text-green-600">
+                  {{ numberWithSpaces(t.price) }}
+                </dd>
+              </div>
+              <div class="w-full border-t border-gray-200"></div>
+              <button
+                @click.stop="handleDelete(t)"
+                class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+              >
+                <svg
+                  class="h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#718096"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  ></path></svg
+                >Удалить
+              </button>
+            </div>
+          </transition-group>
+          <hr class="w-full border-t border-gray-600 my-4" />
+        </template>
       </transition-group>
-      <section v-if="sel" class="relative">
+      <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ sel.name }} - {{ choosedCurrency }}
+          {{ selectedTicker.name }} - {{ choosedCurrency }}
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div
-            v-for="(bar, idx) in normalizeGraph()"
+            v-for="(bar, idx) in normalizedGraph"
             :key="idx"
             :style="{ height: `${bar}%` }"
             class="bg-purple-800 border w-10"
           ></div>
         </div>
         <button
-          @click="sel = null"
+          @click="selectedTicker = null"
           type="button"
           class="absolute top-0 right-0"
         >
@@ -217,18 +220,17 @@ export default {
   data() {
     return {
       ticker: "",
-      currencies: ['USD', 'RUB', 'EUR', 'JPY'],
+      currencies: ["USD", "RUB", "EUR", "JPY"],
       tickers: [],
-      sel: null,
+      selectedTicker: null,
       graph: [],
       coinsList: [],
       possibleCoins: [],
       error: false,
       page: 1,
-      hasNextPage: true,
       image: "",
       sorting: "",
-      choosedCurrency: 'USD',
+      choosedCurrency: "USD",
       filterName: "",
       filterPrice: {
         max: 0,
@@ -263,21 +265,14 @@ export default {
   watch: {
     filterName() {
       this.page = 1;
-
-      const { pathname } = window.location;
-      window.history.pushState(
-        null,
-        document.title,
-        `${pathname}?filter=${this.filterName}&page=${this.page}`
-      );
     },
 
-    page() {
+    pageStateOptions(value) {
       const { pathname } = window.location;
       window.history.pushState(
         null,
         document.title,
-        `${pathname}?filter=${this.filterName}&page=${this.page}`
+        `${pathname}?filter=${value.filterName}&page=${value.page}`
       );
     },
 
@@ -316,13 +311,26 @@ export default {
           this.tickers = this.tickers.sort((a, b) => b.price - a.price);
       }
 
-      this.filteredTickers();
-      localStorage.setItem("tickers", JSON.stringify(this.tickers));
+      localStorage.setItem("tickers", JSON.stringify(this.filteredTickers));
       this.page = 1;
     },
 
     error() {
-      setTimeout(()=> this.error = false, 3500);
+      setTimeout(() => (this.error = false), 3500);
+    },
+
+    paginatedTickers() {
+      if (this.paginatedTickers.length === 0 && this.page > 1) {
+        this.page -= 1;
+      }
+    },
+
+    selectedTicker() {
+      this.graph = [];
+    },
+
+    tickers() {
+      localStorage.setItem("tickers", JSON.stringify(this.tickers));
     }
   },
 
@@ -331,17 +339,14 @@ export default {
       value === "click" || value === "enter" ? (value = this.ticker) : "";
 
       if (this.canAddTicker(value)) {
-        console.log(111)
         const currentTicker = {
           name: value || this.ticker,
           price: "-",
           image: "",
-          currency: this.choosedCurrency,
+          currency: this.choosedCurrency
         };
 
-        this.tickers.push(currentTicker);
-
-        localStorage.setItem("tickers", JSON.stringify(this.tickers));
+        this.tickers = [...this.tickers, currentTicker];
 
         this.updateTickerPrices(currentTicker.name);
         this.filterName = "";
@@ -368,13 +373,15 @@ export default {
           const tickerToUpdate = this.tickers.find(t => t.name === tickerName);
           if (tickerToUpdate) {
             tickerToUpdate.price =
-              data[`${this.choosedCurrency}`] > 1 ? +data[`${this.choosedCurrency}`].toFixed(2) : +data[`${this.choosedCurrency}`].toPrecision(2);
+              data[`${this.choosedCurrency}`] > 1
+                ? +data[`${this.choosedCurrency}`].toFixed(2)
+                : +data[`${this.choosedCurrency}`].toPrecision(2);
             tickerToUpdate.image =
               "https://www.cryptocompare.com" +
               this.coinsList[`${tickerName}`].ImageUrl;
           }
 
-          if (this.sel?.name === tickerName) {
+          if (this.selectedTicker?.name === tickerName) {
             this.graph.push(data.USD);
           }
         } catch (e) {
@@ -384,40 +391,16 @@ export default {
       this.ticker = "";
     },
 
-    filteredTickers() {
-      const start = (this.page - 1) * 6;
-      const end = this.page * 6;
-
-      const filteredTickersList = this.tickers.filter(ticker =>
-        ticker.name.includes(this.filterName)
-      );
-
-      this.hasNextPage = filteredTickersList.length > end;
-
-      return filteredTickersList.slice(start, end);
-    },
-
     select(ticker) {
-      this.sel = ticker;
-      this.graph = [];
+      this.selectedTicker = ticker;
     },
 
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter(t => t !== tickerToRemove);
-      this.graph = [];
-      localStorage.clear();
-      localStorage.setItem(
-        "tickers",
-        JSON.stringify(this.tickers.filter(t => t !== tickerToRemove))
-      );
-    },
-
-    normalizeGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-      return this.graph.map(
-        price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
+      if (this.selectedTicker === tickerToRemove) {
+        this.selectedTicker = null;
+      }
+      localStorage.setItem("tickers", JSON.stringify(this.tickers));
     },
 
     shuffleArray(array) {
@@ -471,9 +454,44 @@ export default {
     }
   },
 
-  // computed: {
+  computed: {
+    startIndex() {
+      return (this.page - 1) * 6;
+    },
 
-  // },
+    endIndex() {
+      return this.page * 6;
+    },
+
+    filteredTickers() {
+      return this.tickers.filter(ticker =>
+        ticker.name.includes(this.filterName)
+      );
+    },
+
+    paginatedTickers() {
+      return this.filteredTickers.slice(this.startIndex, this.endIndex);
+    },
+
+    hasNextPage() {
+      return this.filteredTickers.length > this.endIndex;
+    },
+
+    normalizedGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+      return this.graph.map(
+        price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
+
+    pageStateOptions() {
+      return {
+        filterName: this.filterName,
+        page: this.page
+      };
+    }
+  },
 
   mounted() {
     this.getCoinsList();
